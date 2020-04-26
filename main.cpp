df26b6e8cb13466f6ec8a371ad69655cadae9418
jsmit124 <jsmit124@my.westga.edu>
2020-04-26 14:37:51 -0400

    Initial Commit. Closes #2. Closes #3.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+
+int main (int argc, char ** argv)
+{
+  Fl_Window *window;
+  Fl_Box *box;
+
+  window = new Fl_Window (500, 180, "Connect the Numbers by Smith, Williams, and Steinborn");
+  box = new Fl_Box (20, 40, 260, 100, "Hello World!");
+
+  box->box (FL_UP_BOX);
+  box->labelsize (36);
+  box->labelfont (FL_BOLD+FL_ITALIC);
+  box->labeltype (FL_SHADOW_LABEL);
+  window->end ();
+  window->show (argc, argv);
+
+  return(Fl::run());
+}
